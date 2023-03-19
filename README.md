de2bc8277a64951833487712372e4d860771b0f5
Smartbabbe <estherisrael036@gmail.com>
2023-03-19 20:47:39 +0100

    To-do list

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 This is a repo of a simple To-do list practicing introduction to DOM.
 
 Here is the hosted page - https://smartbabbe.github.io/JS-DOM-To-do-list/
+
+Assignment Question
+
+Create a simple todo list that has the following functionalities:
+You can add a new item to the todo list (required)
+You can remove an item from the todo list (required)
+You can edit an item in the todo list (optional)
+
+Some further requirements:
+the input field must be reset when the add button is clicked on 
+prevent empty values from being added to the list
+the todo list must show the most recently added item first on the list
+the todo list should be stored using localStorage (see helpful resources below). So, when the page is reloaded, any item in the todo list will still be displayed on the web page.
--- file.js
+++ file.js
@@ -1,1 +1,92 @@
-document
+// // Getting the inputted task 
+let inputField = document.querySelector("#input");
+
+// // Getting the add button
+let additionButton = document.querySelector("#addButton");
+
+// Getting the div that stores all the list of tasks
+let addItemsPlace = document.querySelector("#tasks")
+
+// CREATING FUNCTION THAT ADDS THE TASKS
+
+function toAddItem (){
+// Getting the inputted task 
+// let inputField = document.querySelector("#input");
+let tasks = inputField.value;
+
+// NOT ADDING EMPTY INPUT FIELD TO THE ARRAY
+    if (tasks.length <= 0){
+        inputField.setAttribute("placeholder", "Please input a task")
+    } else {
+        // creating the div that holds the specific task
+    let divOfTask = document.createElement("div")
+    divOfTask.classList.add("firsttask")
+
+// Creating child div of each task
+// Creating first div, adding class to it and the input type
+    let subDiv1 = document.createElement("div")
+    subDiv1.classList.add("inputs_first")
+    let checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox")
+    checkbox.classList.add("input1")
+    let task = document.createElement("p")
+    task.innerText = tasks
+
+
+// Appending the input and p tag into the first div
+subDiv1.appendChild(checkbox)
+subDiv1.appendChild(task)
+
+// // Creating second div
+    let subDiv2 = document.createElement("div")
+    subDiv2.classList.add("operations")
+    subDiv2.setAttribute("id", "operation")
+    let editbutton = document.createElement("button")
+    let deletebutton = document.createElement("button")
+    editbutton.classList.add("buttons")
+    deletebutton.classList.add("buttons")
+    editbutton.setAttribute("id", "editbutton")
+    deletebutton.setAttribute("id", "deletebutton")
+    // editbutton.append("Edit")
+    deletebutton.append("Delete")
+
+// // Appending the buttons into the second div
+    // subDiv2.appendChild(editbutton)
+    subDiv2.appendChild(deletebutton)
+
+// // Appending the divs into the task holder
+    divOfTask.appendChild(subDiv1)
+    divOfTask.appendChild(subDiv2)
+
+    addItemsPlace.appendChild(divOfTask)
+}
+
+// function strikeThrough(task){
+//         task[i].style.text-decoration = "line-through";
+//     return task
+}
+
+// checkbox.addEventListener("click", strikeThrough)
+
+
+
+// CREATING FUNCTION THAT EDIT THE TASKS
+function toEditItem (){
+
+}
+
+// CREATING FUNCTION THAT DELETES THE TASKS
+function toDeleteItem (){
+
+}
+
+// FUNCTION THAT CLEARS THE INPUT ON ADDING
+function resetInput(){
+    document.getElementById("input").value = "";
+}
+
+
+
+// Adding event listener to the add button
+additionButton.addEventListener("click", toAddItem)
+additionButton.addEventListener("click", resetInput)
